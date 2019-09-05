d0257cfc1e88cd5f19738f494f7b27edd8b7424a
Stefan Brankovikj <stefan.brankovik@gmail.com>
2019-09-05 12:38:38 +0200

    Remove initial unused files from the framework

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,9 +91,7 @@ $app->singleton(
 |
 */
 
-$app->router->group([
-    'namespace' => 'App\Http\Controllers',
-], function ($router) {
+$app->router->group([], function ($router) {
     require __DIR__.'/../routes/web.php';
 });
 
